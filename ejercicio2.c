3e30fc9e5da4ea3b2f2d68d8baf88926a4b24f6e
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-04-03 14:45:11 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,18 @@
+//Janis Lizeth Salgado Perez//
+//practica 8, ejercicio 2. //
+#include<stdio.h>
+int main()
+{
+    int res=0,n,i=1;
+    do
+    {
+        printf( "ingrese un número, va an la ronda %i: ",1);
+        scanf("%d", &n);
+        res=res+n;
+        i=i+1;
+    }
+    while(suma<=100 && i<10)
+    { 
+        if(suma)
+    
+}
